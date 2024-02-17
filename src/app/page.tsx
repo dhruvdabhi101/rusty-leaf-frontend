eee104c6eda39536f1a0b1cd48e9af041cb00da6
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-02-17 18:17:04 +0530

    fix: automatic redirection to login page for landing page fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import { useEffect } from "react";
 export default function Home() {
     const { push } = useRouter();
     useEffect(() => {
-        if (!localStorage.getItem("token")) {
-            push("/login")
+        if (localStorage.getItem("token")) {
+            push("/home")
         }
     }, [])
     return (
