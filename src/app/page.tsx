a613cd08b7b1af7dd3cf9141a435a2aeffb1d465
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-02-18 17:43:09 +0530

    fix: fixed dots and elliptic mask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,27 +14,25 @@ export default function Home() {
         }
     }, [])
     return (
-        <main className="flex min-h-screen flex-col justify-between p-10 dark:bg-black">
-            <div className="flex flex-row justify-end">
-                <Link href={"/register"}><Button>Sign Up</Button></Link>
-            </div>
-            <DotBackgroundDemo>
-            <div className="flex flex-col justify-center items-center gap-7 w-full h-full">
-                <div className="flex flex-col items-center justify-center gap-5">
-                    <p className="font-semibold text-4xl md:text-6xl lg:text-7xl"> Rusty-Leaf </p>
-                    <p className="font-light text-gray-400 text-md md:text-xl text-center">Best Way to deploy your darkdown with a single click of a button</p>
+        <DotBackgroundDemo>
+            <main className="flex min-h-screen flex-col justify-between p-10">
+                <div className="flex flex-row justify-end">
+                    <Link href={"/register"}><Button>Sign Up</Button></Link>
                 </div>
-                <div className="flex flex-col gap-6 md:flex-row">
-                    <Button variant={"outline"} className="bg-black hover:text-black hover:bg-white "> Let's Deploy </Button>
-                    <Button variant={"ghost"} className="bg-black hover:text-black hover:bg-white">Learn More &rarr;</Button>
+                <div className="flex flex-col justify-center items-center gap-7 w-full h-full">
+                    <div className="flex flex-col items-center justify-center gap-5">
+                        <p className="font-semibold text-4xl md:text-6xl lg:text-7xl"> Rusty-Leaf </p>
+                        <p className="font-light text-gray-400 text-md md:text-xl text-center">Best Way to deploy your darkdown with a single click of a button</p>
+                    </div>
+                    <div className="flex flex-col gap-6 md:flex-row">
+                        <Button variant={"outline"} className="bg-black hover:text-black hover:bg-white "> Let's Deploy </Button>
+                        <Button variant={"ghost"} className="bg-black hover:text-black hover:bg-white">Learn More &rarr;</Button>
+                    </div>
                 </div>
-            </div>
-            </DotBackgroundDemo>
-
-            <div className="flex flex-row justify-center text-sm text-gray-400">
-                Made With Love &lt;3 by Dhruv and Dhyey
-            </div>
-
-        </main>
+                <div className="flex flex-row justify-center text-sm text-gray-400">
+                    Made With Love &lt;3 by Dhruv and Dhyey
+                </div>
+            </main>
+        </DotBackgroundDemo>
     );
 }
