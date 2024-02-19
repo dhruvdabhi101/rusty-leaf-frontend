1bc8384aa07970527b1e299994dd668836cd9688
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-02-19 12:49:30 +0530

    feat: added editor

--- src/app/home/page/[id]/page.tsx
+++ src/app/home/page/[id]/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+import Editor, { Monaco } from "@monaco-editor/react"
+import { useEffect } from "react"
 export default function Page() {
     return (
         <>
-            hello
+            <Editor
+                height={"90vh"}
+                width={"50%"}
+                theme="vs-dark"
+                defaultLanguage="darkdown"
+                path="main.dd"
+            />
         </>
     )
 }
