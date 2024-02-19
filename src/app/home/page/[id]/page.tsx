cde991ded2266a4ff8f3ea4631640fdf5119d9f1
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-02-19 15:50:10 +0530

    feat: editor working now

--- src/app/home/page/[id]/page.tsx
+++ src/app/home/page/[id]/page.tsx
@@ -1,16 +1,37 @@
 "use client"
 import Editor, { Monaco } from "@monaco-editor/react"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { Converter } from "darkdown-ts"
 export default function Page() {
+    const [textData, setTextData] = useState("");
+    const converter = new Converter();
     return (
         <>
+            <div className="flex flex-row h-full w-full gap-5">
             <Editor
                 height={"90vh"}
                 width={"50%"}
                 theme="vs-dark"
                 defaultLanguage="darkdown"
+                onChange={(e) => setTextData(converter.convertToHtml(e))}
                 path="main.dd"
             />
+                <div className="w-full">
+                    Preview
+
+                <div className='
+            p-4
+                w-fit
+                prose-h1:text-2xl prose-h1:font-bold
+                prose-li:text-md prose-li:font-thin
+                prose-a:text-neutral-400 prose-a:hover:underline prose-a:italic
+            tracking-wider leading-7
+            '>
+                    <p dangerouslySetInnerHTML={{ __html: textData }}>
+                    </p>
+                </div>
+                </div>
+            </div>
         </>
     )
 }
